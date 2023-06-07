eae0506b8b7ad1f30a085f85d1b293fbf5c5c8ea
bindutata <133663572+bindutata@users.noreply.github.com>
2023-06-07 23:01:47 +0530

    Add files via upload

---
+++ 6-6-23.py
@@ -0,0 +1,43 @@
+# program to take tuple values fromkeyboard and print sum and average
+
+#n=int(input("enter the value:"))
+#list=[]
+#for x in range(n):
+    #element=int(input("enter some number:"))
+    #list.append(element)
+#t=tuple(list)
+#print(t)
+#n=len(t)
+#sum=0
+#for x in t:
+    #sum=sum+(x)
+#print("sum of tuple values:",sum)
+#print("average of values is:",sum/n)
+
+
+#user_input=input("enter the tuple")
+#user_tuple=tuple(user_input.split(','))
+#print(user_tuple)
+#sum=0
+#n=len(user_tuple)
+#for x in user_tuple:
+#    sum=sum+int(x)
+#print("the sum of tuple values is:",sum)
+#print("the avg of tuple values is:",sum/n)
+
+
+a=eval(input("enter the values"))
+sum=0
+n=len(a)
+for x in a:
+    sum=sum+(x)
+print("the sum of tuple values is:",sum)
+print("the avg of tuple values is:",sum/n)
+
+
+
+
+
+
+
+
